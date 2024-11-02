5b4ce5a210a0ed72f0f8f0cfa8b6c1a9af58e84a
Khanh Duy <lukiluna23803@gmail.com>
2024-11-02 20:35:30 +0700

    fix all 70

---
+++ Frontend Projects/03-personal-portfolio/script.js
@@ -0,0 +1,7 @@
+document.getElementById('theme-toggle').addEventListener('click', function(){
+    document.body.classList.toggle('dark-mode');
+
+    const icon = this.querySelector('i');
+    icon.classList.toggle('fa-sun');
+    icon.classList.toggle('fa-moon');
+});;
