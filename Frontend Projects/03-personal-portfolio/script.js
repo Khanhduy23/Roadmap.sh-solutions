2f2b9f33661522a07a21713eb6410f798b4ed22c
Khanh Duy <lukiluna23803@gmail.com>
2024-11-04 17:01:15 +0700

    Done responsive 1-4

--- Frontend Projects/03-personal-portfolio/script.js
+++ Frontend Projects/03-personal-portfolio/script.js
@@ -1,7 +1,23 @@
-document.getElementById('theme-toggle').addEventListener('click', function(){
-    document.body.classList.toggle('dark-mode');
+document.addEventListener("DOMContentLoaded", () => {
+    const emailLinks = document.querySelectorAll("a");
 
-    const icon = this.querySelector('i');
-    icon.classList.toggle('fa-sun');
-    icon.classList.toggle('fa-moon');
-});;
+    emailLinks.forEach((emailLink) => {
+        emailLink.addEventListener("click", (event) => {
+            event.preventDefault();
+            alert("Email feature is disabled for demo purposes.");
+        });
+    });
+
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', function(){
+            document.body.classList.toggle('dark-mode');
+
+            const icon = this.querySelector('i');
+            if (icon) {
+                icon.classList.toggle('fa-sun');
+                icon.classList.toggle('fa-moon');
+            }
+        });
+    }
+});
