7faf3a86aded01efdbbacda44ba80af80e712171
Khanh Duy <lukiluna23803@gmail.com>
2024-11-07 19:11:04 +0700

    fix

--- Frontend Projects/01-single-page-cv/script.js
+++ Frontend Projects/01-single-page-cv/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const emailLink = document.querySelector("address a");
-    emailLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        alert("Email feature is disabled for demo purposes.");
-    });
+  const emailLink = document.querySelector("address a");
+  emailLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    alert("Email feature is disabled for demo purposes.");
+  });
 });
